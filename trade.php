4069238dc008082c2d4b76a8c07ce31fce363e02
Guilhem.Poties <gpoties@gmail.com>
2023-03-06 10:26:06 +0100

    reglement

---
+++ trade.php
@@ -0,0 +1,11 @@
+<?php $title="Troc"; include "modules/head.php"; ?>
+<?php $page="settlement"; include "modules/body/header.php";?>
+<!-- Main -->  
+    <main>
+        <h2>Troc</h2>
+        <?php
+            print "<p>Une notification a été envoyée à l'utilisateur pour qu'il confirme le troc</p>";
+            print " <a href='index.php'>Retourner à l'accueil</a>";
+        ?>
+    </main>
+<?php include "modules/body/footer.php"; ?>
